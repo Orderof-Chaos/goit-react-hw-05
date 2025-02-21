80e4de1aee16c048d04efa10806b60d6d3e912c1
Олексій <alesha1279@gmail.com>
2025-02-21 19:05:54 +0200

    CreateAppImport

---
+++ src/App.jsx
@@ -0,0 +1,34 @@
+import { Suspense, useState } from 'react'
+import { Routes, Route} from 'react-router-dom'
+import './App.css'
+
+const HomePage = lazy(() => import('./pages/HomePage'));
+const MoviesPage = lazy(() => import('./pages/MoviesPage'));
+const MovieDetailsPage = lazy(() => import('./pages/MovieDetailsPage'));
+const NotFoundPage = lazy(() => import('./pages/NotFoundPage'));
+const MovieCast = lazy(() => import('./components/MovieCast'));
+const MovieReviews = lazy(() => import('./components/MovieReviews'));
+
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <main>
+      <Navigation/>
+      <Suspense fallback={<div>Loading page...</div>}>
+        <Routes>
+        <Route path='/' element={<HomePage />} />
+        <Route path='/movies' element={<MoviesPage/>} />
+        <Route path='/movies/:movieId' element={<MovieDetailsPage />} >
+          <Route path='cast' element={<MovieCast/>} />
+          <Route path='reviews' element={<MovieReviews/>}/>
+        </Route>
+        <Route path='*' element={<NotFoundPage/>} />
+      </Routes>
+      </Suspense>
+    </main>
+  )
+}
+
+export default App
